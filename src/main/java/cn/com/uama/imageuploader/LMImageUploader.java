eb46f4b289c863ad83cb5ad6ace3debf82c745a9
liwei <liwei@uama.com.cn>
2017-07-04 09:32:14 +0800

    change File.pathSeparator to File.separator

--- src/main/java/cn/com/uama/imageuploader/LMImageUploader.java
+++ src/main/java/cn/com/uama/imageuploader/LMImageUploader.java
@@ -60,7 +60,7 @@ public class LMImageUploader {
 
         OkHttpClient client = clientBuilder.build();
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(uploadUrl.endsWith(File.pathSeparator) ? uploadUrl : uploadUrl + File.pathSeparator)
+                .baseUrl(uploadUrl.endsWith(File.separator) ? uploadUrl : uploadUrl + File.pathSeparator)
                 .client(client)
                 .addConverterFactory(LMGsonConverterFactory.create(BaseBean.class))
                 .build();
