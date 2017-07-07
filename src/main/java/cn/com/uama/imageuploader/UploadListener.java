68d06c23c8b02887be9d6605e0f4cf0663390ad8
liwei <liwei@uama.com.cn>
2017-07-07 20:50:14 +0800

    add method comment for UploadListener interface.

--- src/main/java/cn/com/uama/imageuploader/UploadListener.java
+++ src/main/java/cn/com/uama/imageuploader/UploadListener.java
@@ -6,6 +6,18 @@ package cn.com.uama.imageuploader;
  * Description: 上传回调接口
  */
 public interface UploadListener {
+    /**
+     * successful uploading callback
+     *
+     * @param imageUrls image url array of JSON format
+     */
     void onSuccess(String imageUrls);
+
+    /**
+     * failed uploading callback
+     *
+     * @param errorCode    code representing error type
+     * @param errorMessage error message
+     */
     void onError(String errorCode, String errorMessage);
 }
